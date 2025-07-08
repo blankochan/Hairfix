1ced8ab57d02f776a2696c44ef68c2ae0b078eaa
blankochan <liz@blanky.dev>
2025-07-07 21:37:54 -0500

    fix: remove the log spam

--- Hairfix/HairfixClass.cs
+++ Hairfix/HairfixClass.cs
@@ -7,21 +7,14 @@ using Hairfix;
 [assembly: MelonGame("Buckethead Entertainment", "RUMBLE")]
 [assembly: MelonColor(255, 255, 170, 238)] // #FAE pink :3
 
-namespace Hairfix // worlds simplist fucking mod
+namespace Hairfix; // worlds simplist fucking mod
+public class HairfixClass : MelonMod
 {
-    public class HairfixClass : MelonMod
+    private const string comment = "simpliest bug ever qwq";
+    public override void OnSceneWasLoaded(int _, string sceneName)
     {
-        private readonly Color32 cullColor = new(255, 0, 0, 255);
-
-        private const string comment = "simpliest bug ever qwq";
-        public override void OnSceneWasLoaded(int buildIndex, string sceneName)
-        {
-            if (sceneName == "Loader") return;
-            Mesh frenchBob = Resources.FindObjectsOfTypeAll<Mesh>().First(mesh => mesh.name == "Top7");
-            
-            frenchBob.colors32 = Enumerable.Repeat(cullColor, frenchBob.colors32.Length).ToArray();
-            LoggerInstance.Msg("Patched French Bob");
-        }
-        
+        if (sceneName == "Loader") return;
+        Mesh frenchBob = Resources.FindObjectsOfTypeAll<Mesh>().First(mesh => mesh.name == "Top7");
+        frenchBob.colors32 = Enumerable.Repeat(new Color32(255, 0, 0, 255), frenchBob.colors32.Length).ToArray();
     }
 }
